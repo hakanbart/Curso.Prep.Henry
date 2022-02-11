3907e0f6966fb56d61b0bcdac5b4e9b22d443250
Sergio Sebastian <95728967+hakanbart@users.noreply.github.com>
2022-02-11 11:34:47 +0100

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,12 +1,12 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "hello";
+const nuevaString = "Holas";
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 8;
 
 // Crea una variable booleana:
-const nuevoBool = false ;
+const nuevoBool = true ;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
